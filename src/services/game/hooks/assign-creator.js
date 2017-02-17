7d32415cf6063a2288f4997f3dd4033f9f68108a
StefanKollaart <garbera@icloud.com>
2017-02-17 13:47:50 +0100

    Fixed creating

--- src/services/game/hooks/assign-creator.js
+++ src/services/game/hooks/assign-creator.js
@@ -2,7 +2,7 @@
 
 module.exports = function(options) {
   return function assignCreator(hook) {
-    const user = hook.params.creator;
+    const user = hook.data.creator;
     hook.data.creatorId = user._id;
   }
 }
